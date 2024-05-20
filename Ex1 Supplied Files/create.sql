44c3b370d779921462d01844dccb8dd6f037fdf4
ramGoldstein <ram.goldstein.43@gmail.com>
2024-05-20 16:53:57 +0300

    start of create sql

--- Ex1 Supplied Files/create.sql
+++ Ex1 Supplied Files/create.sql
@@ -1,32 +1,62 @@
-create table oscars(
-  id varchar,
+create table FilmStudio(
+  name varchar
+);
+
+create table Film(
+  film_id varchar UNIQUE NOT NULL,
   film_name varchar,
-  oscar_year varchar,
-  studio varchar,
-  award varchar,
+  imdb_rating varchar,
+  imdb_votes varchar,
+  content_rating varchar,
   release_year varchar,
   duration varchar,
   genres varchar,
   imdb_rating varchar,
   imdb_votes varchar,
   content_rating varchar,
-  directors varchar,
-  authors varchar,
-  actors varchar,
-  filmId varcharfff
-);
-create table Film Studio(
-  name varchar
-);
-
-create table Film(
-  filmId varchar
+  studio varchar,
+  oscar_year varchar,
+  award varchar,
+  Foreign KEY(studio) REFERENCES FilmStudio(name)
 );
 
 create table Participant(
+  name varchar UNIQUE NOT NULL
+);
 
+create table Author(
+  name varchar,
+  Foreign KEY(name) REFERENCES Director(name)
+);
+
+create table WrittenBy(
+  film_id varchar,
+  name varchar,
+  Foreign KEY(film_id) REFERENCES Film(film_id),
+  Foreign KEY(name) REFERENCES Film(name),
+);
+
+create table Actor(
+  name varchar,
+  Foreign KEY(name) REFERENCES Director(name)
+);
+
+create table ActedBy(
+  film_id varchar,
+  name varchar,
+  Foreign KEY(film_id) REFERENCES Film(film_id),
+  Foreign KEY(name) REFERENCES Film(name),
 );
 
 create table Director(
-
+  name varchar,
+  Foreign KEY(name) REFERENCES Director(name)
 );
+
+create table DirectedBy(
+  film_id varchar,
+  name varchar,
+  Foreign KEY(film_id) REFERENCES Film(film_id),
+  Foreign KEY(name) REFERENCES Film(name),
+);
+
