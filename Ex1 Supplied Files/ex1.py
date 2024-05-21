623b5f7fb0bdedd33f098db78dae7220d2220f91
ramGoldstein <ram.goldstein.43@gmail.com>
2024-05-22 00:13:25 +0300

    drop

--- Ex1 Supplied Files/ex1.py
+++ Ex1 Supplied Files/ex1.py
@@ -1,28 +1,24 @@
 import csv
 import pandas as pd
-from io import TextIOWrapper
 from zipfile import ZipFile
-
-# opens file.
-oscars_outfile = open("oscars.csv", 'w' , encoding='UTF8',newline='')
-oscars_outwriter = csv.writer(oscars_outfile, delimiter=",", quoting=csv.QUOTE_MINIMAL)
 
 
 # process_file goes over all rows in original csv file, and sends each row to process_row()
 def process_file():
-  # with ZipFile('oscars.zip') as zf:
-  #     with zf.open('oscars.csv', 'r') as infile:
-  #         reader = csv.reader(TextIOWrapper(infile, 'utf-8'))
-  #         for row in reader:
-  #             # TO DO splits row into the different csv table files
-  #             oscars_outwriter.writerow(row)
-  # oscars_outfile.close()
-  df = pd.read_csv('oscars.zip')
-  load_directed_by(df)
+    with ZipFile('oscars.zip') as zf:
+        with zf.open('oscars.csv', 'r') as infile:
+            df = pd.read_csv(infile)
+    load_simple_csv(df,)
 
-def load_directed_by(df):
-    selected_columns = df[['Film ID', 'Year of Release']].dropna()
-    selected_columns.to_csv('directed_by.csv', index=False)
+def load_simple_csv(main_df ,list_columns_from_zip, names_columns_csv, name_csv:str):
+    selected_columns = main_df[list_columns_from_zip].dropna().drop_duplicates()
+    selected_columns.rename(columns=names_columns_csv)
+    selected_columns.to_csv(name_csv, index=False)
+
+def load_columns_list(main_df ,list_columns_from_zip, columns_to_explode, names_columns_csv, name_csv:str):
+    df_exploded = main_df.explode(columns_to_explode).reset_index(drop=True)
+    load_simple_csv(df_exploded, list_columns_from_zip, names_columns_csv, name_csv)
+
 
 def get_names():
     return ["Actor", "Director", "Author", "Film Studio", "Film"]
