96ec8a10e0df6ee64dbd2d7320564665af6ab194
ramGoldstein <ram.goldstein.43@gmail.com>
2024-05-22 00:17:02 +0300

    drop

--- Ex1 Supplied Files/ex1.py
+++ Ex1 Supplied Files/ex1.py
@@ -2,13 +2,14 @@ import csv
 import pandas as pd
 from zipfile import ZipFile
 
-
+DIRECTED_BY_COLUMNS = ['Film ID', 'Directors']
+DIRECTEDBY_NAMES_SQL = {'Film ID': 'film_id', 'Directors': 'name'}
 # process_file goes over all rows in original csv file, and sends each row to process_row()
 def process_file():
     with ZipFile('oscars.zip') as zf:
         with zf.open('oscars.csv', 'r') as infile:
             df = pd.read_csv(infile)
-    load_simple_csv(df,)
+    load_simple_csv(df, DIRECTED_BY_COLUMNS, DIRECTEDBY_NAMES_SQL, "FilmStudio")
 
 def load_simple_csv(main_df ,list_columns_from_zip, names_columns_csv, name_csv:str):
     selected_columns = main_df[list_columns_from_zip].dropna().drop_duplicates()
