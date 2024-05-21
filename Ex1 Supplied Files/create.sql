2f3e323be71cf3bc6652d22dd8b16af60f117990
RomFaibish <133245124+RomFaibish@users.noreply.github.com>
2024-05-21 23:19:29 +0300

    check votes positive

--- Ex1 Supplied Files/create.sql
+++ Ex1 Supplied Files/create.sql
@@ -7,7 +7,7 @@ create table Film(
   film_id varchar(100) UNIQUE NOT NULL,
   film_name varchar(100),
   imdb_rating float CHECK(imdb_rating >= 0.0 and imdb_rating <= 10.0),
-  imdb_votes int,
+  imdb_votes int CHECK(imdb_votes >= 0),
   content_rating varchar CHECK(content_rating == "G" or content_rating == "PG" or
   content_rating == "PG-13" or content_rating == "R" or content_rating == "NR"),
   release_year int CHECK(release_year > 0),
